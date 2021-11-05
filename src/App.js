0b764fd4c80dca7f400e2cd140df509b53c970bb
Sergiy Bubenko <bubenko.s91@gmail.com>
2021-11-06 00:46:14 +0200

    preparatory work is over

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Component } from "react";
 import "./App.css";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 import Searchbar from "./components/Searchbar";
+import ImageGalleryItem from "./components/ImageGalleryItem";
+import Button from "./components/Button";
+import Loader from "react-loader-spinner";
+
 import Modal from "./components/Modal";
 
 class App extends Component {
@@ -22,6 +27,15 @@ class App extends Component {
     return (
       <div className="App">
         <Searchbar onSubmit={this.Searchbar} />
+        <ImageGalleryItem />
+        <Button />
+        <Loader
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={3000} //3 secs
+        />
         {this.state.showModal && (
           <Modal onClose={this.toggleModal}>
             <h1>то что будет отображено в модалке</h1>
---
+++ src/components/Button/Button.js
@@ -0,0 +1,12 @@
+function Button() {
+  return <button type="button">Load more</button>;
+}
+
+export default Button;
+
+// // Для скрола используй следующий код:
+
+// window.scrollTo({
+//   top: document.documentElement.scrollHeight,
+//   behavior: 'smooth',
+// });
